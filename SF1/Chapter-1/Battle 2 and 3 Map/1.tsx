1906eefa64b3f8b493a9f08ee3f1f2f36821c262
feldc <feldc@protonmail.com>
2021-08-06 13:53:56 -0400

    Add: Start: SF1 Chapter 1 battle 2 and 3 map

---
+++ SF1/Chapter-1/Battle 2 and 3 Map/1.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="1" tilewidth="4" tileheight="4" tilecount="1024" columns="32">
+ <image source="../../../../ShiningForceRemade/Assets/SF1/Tilesets/1.png" width="128" height="128"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="WalkablePath" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="391" wangid="0,0,0,1,1,0,0,0"/>
+   <wangtile tileid="392" wangid="0,0,0,0,1,1,0,0"/>
+   <wangtile tileid="393" wangid="0,0,0,1,1,0,0,0"/>
+   <wangtile tileid="394" wangid="0,0,1,1,1,1,0,0"/>
+   <wangtile tileid="395" wangid="0,0,0,1,1,1,1,0"/>
+   <wangtile tileid="422" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="423" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="424" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="425" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="426" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="427" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="452" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="453" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="455" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="456" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="457" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="484" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="485" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="487" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="488" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="489" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
