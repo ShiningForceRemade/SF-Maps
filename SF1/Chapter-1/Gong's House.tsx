f0ddf8a21673e9e27c35c46c6f29d3c852e6c48b
feldc <feldc@protonmail.com>
2021-09-06 13:20:06 -0400

    Add: SF1 Chapter 1 - Gong's house

---
+++ SF1/Chapter-1/Gong's House.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Gong's House" tilewidth="553" tileheight="648" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="553" height="648" source="../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter1-RunefaustInvasion-Cabin.png"/>
+ </tile>
+</tileset>
