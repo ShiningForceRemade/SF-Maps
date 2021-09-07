a90b18eb6eb727b48a6b775fde5b424bd9842059
feldc <feldc@protonmail.com>
2021-09-07 02:45:21 -0400

    Add: Start: SF1 - Chapter 1 - Alterone map

---
+++ SF1/Chapter-1/Alterone/AlteroneRef.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="AlteroneRef" tilewidth="1872" tileheight="1968" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1872" height="1968" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter1-RunefaustInvasion-Alterone.png"/>
+ </tile>
+</tileset>
