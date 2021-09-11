b2fef9179d6e01607bc05d11718eedaff60c24ca
feldc <feldc@protonmail.com>
2021-09-11 18:34:23 -0400

    Add: SF1 - Chapter 1 - Battle 1 map (recreated in tiled from sf-godot)

---
+++ SF1/Chapter-1/Battle 1 Map/Battle_1_Ref.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Battle_1_Ref" tilewidth="384" tileheight="744" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="384" height="744" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter1-RunefaustInvasion-Ancients'Gate.png"/>
+ </tile>
+</tileset>
