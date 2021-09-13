43ce4dea3b7d21ecf65a2eda4853d82c94e207d7
feldc <feldc@protonmail.com>
2021-09-13 14:52:59 -0400

    Add: SF1 - Chapter 1 - Battle 2-3 Map

--- SF1/Chapter-1/Battle 2 and 3 Map/Battle2And3Ref.tsx
+++ SF1/Chapter-1/Battle 2 and 3 Map/Battle2And3Ref.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="Battle2And3Ref" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.5" tiledversion="1.7.1" name="Battle2And3Ref" tilewidth="768" tileheight="888" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="768" height="888" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter1-RunefaustInvasion.png"/>
+ </tile>
+</tileset>
