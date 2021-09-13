127b4c11bbc90884a4324f2bd2e01bcae3130aa0
feldc <feldc@protonmail.com>
2021-09-13 17:01:02 -0400

    Add: Start: SF1 - Chapter 1 - Guardiana Town destroyed (with ref)

---
+++ SF1/Chapter-1/Guardiana/TownRef.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="TownRef" tilewidth="1584" tileheight="3168" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1584" height="3168" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter1-RunefaustInvasion-Guardiana.png"/>
+ </tile>
+</tileset>
