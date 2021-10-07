480b3f63ca449b940c650d084a7eea3bce065418
feldc <feldc@protonmail.com>
2021-10-07 15:38:08 -0400

    Add: Setup: SF1 - Chapter 2 - Overworld map

---
+++ SF1/Chapter-2/Overworld/C2-Overworld-Ref.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="C2-Overworld-Ref" tilewidth="768" tileheight="888" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="768" height="888" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter2-SpiritOfTheHolySpring.png"/>
+ </tile>
+</tileset>
